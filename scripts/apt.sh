4a92be397ac43741258f04397702f004fc853656
nervo <nervo@nervo.net>
2015-10-18 13:58:14 +0200

    Force httpredir and contrib/non-free by script

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+cat <<EOF > /etc/apt/sources.list
+deb http://httpredir.debian.org/debian wheezy main non-free contrib
+deb-src http://httpredir.debian.org/debian wheezy main non-free contrib
+
+deb http://security.debian.org/ wheezy/updates main contrib non-free
+deb-src http://security.debian.org/ wheezy/updates main contrib non-free
+
+# wheezy-updates, previously known as 'volatile'
+deb http://httpredir.debian.org/debian wheezy-updates main contrib non-free
+deb-src http://httpredir.debian.org/debian wheezy-updates main contrib non-free
+EOF
+
 apt-get update
